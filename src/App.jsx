d3063c204d080ffc6b865a881f297df8685587e9
nikcladis <nikcladis@gmail.com>
2023-12-15 18:00:00 +0200

    added PageLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
-import UserForm from "./components/UserForm/UserForm";
-import UsersList from "./components/UsersList/UsersList";
+import PageLayout from "./layouts/PageLayout";
 
 const App = () => {
   return (
     <>
-      <header className="p-2 bg-gray-300">
-        <h1 className="font-bold">CRUD Application</h1>
-      </header>
-      <main className="flex flex-col sm:flex-row">
-        <div className="w-full xl:w-1/4 lg:w-2/5 md:w-1/2 p-2">
-          <UserForm />
-        </div>
-        <div className="w-full xl:w-3/4 lg:w-3/5 md:w-1/2 p-2 bg-gray-700">
-          <UsersList />
-        </div>
-      </main>
+      <PageLayout />
     </>
   );
 };
---
+++ src/layouts/PageLayout.jsx
@@ -0,0 +1,22 @@
+import UserForm from "../components/UserForm/UserForm";
+import UsersList from "../components/UsersList/UsersList";
+
+const PageLayout = () => {
+  return (
+    <>
+      <header className="p-2 bg-gray-300">
+        <h1 className="font-bold">CRUD Application</h1>
+      </header>
+      <main className="flex flex-col sm:flex-row">
+        <div className="w-full xl:w-1/4 lg:w-2/5 md:w-1/2 p-2">
+          <UserForm />
+        </div>
+        <div className="w-full xl:w-3/4 lg:w-3/5 md:w-1/2 p-2 bg-gray-700">
+          <UsersList />
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default PageLayout;
