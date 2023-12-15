72b7d4eb0f6a5e1e504cd06901691cb668d5dc4c
nikcladis <nikcladis@gmail.com>
2023-12-15 21:31:22 +0200

    implemented props for UserCard

--- src/components/UsersList/UserCard.jsx
+++ src/components/UsersList/UserCard.jsx
@@ -1,5 +1,22 @@
-const UserCard = () => {
-  return <article>UserCard</article>;
+const UserCard = ({
+  name,
+  username,
+  email,
+  address: { city, zipcode },
+  phone,
+}) => {
+  return (
+    <article>
+      <ul className="p-5 flex flex-col gap-2 border-2 border-blue-700 cursor-pointer rounded-md transition duration-300 ease-in hover:text-white hover:bg-blue-700">
+        <li>Name: {name}</li>
+        <li>Username: {username}</li>
+        <li>Email: {email}</li>
+        <li>City: {city}</li>
+        <li>Zipcode: {zipcode}</li>
+        <li>Phone: {phone}</li>
+      </ul>
+    </article>
+  );
 };
 
 export default UserCard;
--- src/components/UsersList/UsersList.jsx
+++ src/components/UsersList/UsersList.jsx
@@ -10,6 +10,8 @@ const UsersList = () => {
       try {
         const users = await fetchData(endpoints.users);
 
+        console.log(users);
+
         setUsersData(users);
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -20,18 +22,15 @@ const UsersList = () => {
   }, []);
 
   return (
-    <ul className="flex gap-2">
+    <ul className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4">
       <li>
-        <UserCard />
-      </li>
-      <li>
-        <UserCard />
-      </li>
-      <li>
-        <UserCard />
-      </li>
-      <li>
-        <UserCard />
+        <UserCard
+          name="Nick Kladis"
+          username="nikcladis"
+          email="nikcladis@gmail.com"
+          address={{ city: "Patras", zipcode: "26504" }}
+          phone="6948181847"
+        />
       </li>
     </ul>
   );
